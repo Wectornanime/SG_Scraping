3177c5f5ff21d3cee5289a1c31b1052edbf6258a
Wectornanime <Wectornanime@gmail.com>
2024-02-18 16:39:37 -0300

    added data analysis of fist table

--- table.py
+++ table.py
@@ -2,6 +2,11 @@ import camelot
 from IPython.display import display
 from time import time
 
+pdf_links = [
+    'https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/medicamentos/medicamentos-de-referencia/arquivos/lista-a-incluidos-050124.pdf',
+    'https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/medicamentos/medicamentos-de-referencia/arquivos/lista-a-excluidos-050124.pdf'
+]
+
 # while True:
 #     try:
 #         op = int(input('Escolha uma opção:\n[0] - Inativos\n[1] - Ativos\n->'))
@@ -17,7 +22,7 @@ from time import time
 #     except:
 #         print('Opção inválida, escolha outra')
 
-link_pdf = 'https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/medicamentos/medicamentos-de-referencia/arquivos/lista-a-incluidos-050124.pdf'
+link_pdf = pdf_links[0] # usando os validos
 
 temp_init = time()
 
@@ -40,12 +45,18 @@ for pg in range(0, len(tables)):
     if pg == 0:
         table.df = table.df[2:].reset_index(drop=True)
 
-
-    col = 1
     for row in range(0, len(table.df)):
-        if len(table.df.at[row, col]) == 0:
-            print(f'{table.df.at[row-1, col]} {table.df.at[row+1, col]}')
-            table.df.at[row, col] = f'{table.df.at[row-1, col]} {table.df.at[row+1, col]}'
+        if table.df.at[row, 3] != '':
+            for col in range(0, 7):
+                if table.df.at[row, col] == '':
+                    print(f'{table.df.at[row-1, col]} {table.df.at[row+1, col]}')
+                    table.df.at[row,col] = f'{table.df.at[row-1, col]} {table.df.at[row+1, col]}'
+
+    for row in range(0, len(table.df)):
+        if table.df.at[row, 3] == '':
+            table.df = table.df.drop(index=row)
+        
+    table.df = table.df.reset_index(drop=True)
 
     display(table.df)
 
@@ -53,4 +64,4 @@ for pg in range(0, len(tables)):
 
 
 temp_exec = time() - temp_init
-print(f'\n\nTempo de execução: {temp_exec:.5f}s')
+# print(f'\n\nTempo de execução: {temp_exec:.5f}s')
