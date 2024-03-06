5d7654c098176d34a3de2fb0c3068b1d72987016
Wectornanime <Wectornanime@gmail.com>
2024-03-06 13:20:37 -0300

    Update table.py

--- table.py
+++ table.py
@@ -4,7 +4,9 @@ from time import time
 
 pdf_links = [
     'https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/medicamentos/medicamentos-de-referencia/arquivos/lista-a-incluidos-050124.pdf',
-    'https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/medicamentos/medicamentos-de-referencia/arquivos/lista-a-excluidos-050124.pdf'
+    'https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/medicamentos/medicamentos-de-referencia/arquivos/lista-a-excluidos-050124.pdf',
+    'https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/medicamentos/medicamentos-de-referencia/arquivos/lista-b-incluidos-050124.pdf',
+    'https://www.gov.br/anvisa/pt-br/setorregulado/regularizacao/medicamentos/medicamentos-de-referencia/arquivos/lista-b-excluidos-050124.pdf'
 ]
 
 # while True:
@@ -24,17 +26,38 @@ pdf_links = [
 
 def filterTable(temp_table):
     for row in range(0, len(temp_table.df)):
-        if temp_table.df.at[row, 3] != '':
+        if temp_table.df.at[row, 3] != '' and '/' not in temp_table.df.at[row, 3]:
             for col in range(0, len(temp_table.df.columns)):
                 if temp_table.df.at[row, col] == '':
                     # print(f'{temp_table.df.at[row-1, col]} {temp_table.df.at[row+1, col]}')
                     temp_table.df.at[row,col] = f'{temp_table.df.at[row-1, col]} {temp_table.df.at[row+1, col]}'
 
     for row in range(0, len(temp_table.df)):
-        if temp_table.df.at[row, 3] == '':
+        if temp_table.df.at[row, 3] == '' or '/' in temp_table.df.at[row, 3]:
             temp_table.df = temp_table.df.drop(index=row)
         
     temp_table.df = temp_table.df.reset_index(drop=True)
+    
+    return temp_table
+
+def filterTable2(temp_table):
+    for row in range(0, len(temp_table.df)):
+        if temp_table.df.at[row, 3] == '':
+            for col in range(0, len(temp_table.df.columns)):
+                if temp_table.df.at[row, col] != '':
+                    newData = list()
+                    rw = 1
+                    while True:
+                        if temp_table.df.at[row+rw, 3] == '':
+                            rw += 1
+                        else:
+                            break
+                    for temp_row in range(0, rw+rw+1):
+                        newData.append(temp_table.df.at[temp_row, col])
+                        temp_table.df.at[temp_row, col] = ''
+                    
+                    print(' '.join(newData))
+                    temp_table.df.at[row+rw, col] = ' '.join(newData)
     
     return temp_table
 
@@ -47,13 +70,13 @@ def getDatas(ntable):
 
     for pg in range(0, len(tables)):
         if npg == 10:
-            a = input('Enter para continuar')
+            a = input('\nEnter para continuar')
             npg = 0
         
         print('\n\n')
         print('='*100)
         print(f'pag: {pg}')
-        print('='*100, end='\n\n')
+        print('\n\n')
         
         table = tables[pg]
 
@@ -63,6 +86,8 @@ def getDatas(ntable):
                     table.df = table.df[2:].reset_index(drop=True)
                 case 1:
                     table.df = table.df[4:].reset_index(drop=True)
+                case 3:
+                    table.df = table.df[4:].reset_index(drop=True)
 
         table = filterTable(table)
 
@@ -71,7 +96,7 @@ def getDatas(ntable):
         npg += 1
 
 
-getDatas(0) #usando a tabela validos A
+getDatas(1) #usando a tabela validos A
 
 
 
